8a9f89e1d1bf41c77a7e359fe5ae48bc7d17cb4d
Piotr Gucwa <piotr.gucwa@student.pk.edu.pl>
2021-07-30 18:27:20 +0200

    Add routing between components

--- Frontend/src/app/home/home.module.ts
+++ Frontend/src/app/home/home.module.ts
@@ -4,13 +4,14 @@ import { AgmCoreModule } from '@agm/core';
 
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './home.component';
 import {HomeRoutingModule } from './home-routing.module';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { SettingsComponent } from './settings/settings.component';
     HomeComponent,
     NavigationBarComponent,
     ProfileComponent,
-    SettingsComponent],
+    SettingsComponent,
+    StartComponent],
   imports: [ 
     CommonModule,
     HomeRoutingModule,
